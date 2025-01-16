99a27e9a26896b392043fa08abd3367ccd3b19b6
Elkkuss <eliastk@metropolia.fi>
2025-01-16 17:19:14 +0200

    lisäsin moduuli 2 tehtävä 6

---
+++ Moduuli2teht6.py
@@ -0,0 +1,11 @@
+import random
+numero = (random.randint(0, 9))
+yksi = (random.randint(0, 9))
+luku = (random.randint(0, 9))
+eka = (random.randint(1, 6))
+toka = (random.randint(1, 6))
+kolmas = (random.randint(1, 6))
+neljas = (random.randint(1, 6))
+print(numero, yksi, luku)
+print(eka, toka, kolmas, neljas)
+
