ff51365b10984bc06b084f9cca253ac2456a0816
Elkkuss <eliastk@metropolia.fi>
2025-01-16 17:18:24 +0200

    lisäsin moduuli 2 tehtävä 3

---
+++ Moduuli2teht3.py
@@ -0,0 +1,9 @@
+kanta_str = input("Anna Suorakulmiolle kanta: ")
+korkeus_str = input("Anna Suorakulmiolle korkeus: ")
+kanta = float(kanta_str)
+korkeus = float(korkeus_str)
+pinta = (kanta*korkeus)
+piiri = (kanta+korkeus+kanta+korkeus)
+print(" suorakulmion pinta-ala: " + str(pinta))
+print(" suorakulmion piiri: " + str(piiri))
+
