00989f6d60b624291279dd2aeff66381fe727927
Elkkuss <eliastk@metropolia.fi>
2025-01-21 13:04:15 +0200

    lisäsin moduuli 2 tehtävä 6

---
+++ Moduuli3teht1.py
@@ -0,0 +1,9 @@
+kuhat = float(input("Anna kuhan pituus: "))
+pituus = (37 - kuhat)
+if kuhat<=36:
+    print("Laske kuha pois.")
+    print(f"Kuhasi on {pituus} cm liian lyhyt. ")
+else:
+        print("Kuhasi on täysi mittainen")
+
+
