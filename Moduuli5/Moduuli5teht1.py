bb007a1e161800bdc1f4fa773cf9a66f759de73a
Elkkuss <eliastk@metropolia.fi>
2025-01-31 11:36:59 +0200

    lisäsin moduuli 5 tehtävä 1

---
+++ Moduuli5/Moduuli5teht1.py
@@ -0,0 +1,8 @@
+import random
+noppa = int(input("Anna arpakuutioiden määrä: "))
+summa = 0
+for luku in range(noppa):
+    heitto = random.randint(1, 6)
+    print(f"arpakuutio{luku+1}: {heitto}")
+    summa += heitto
+    print(f"Arpakuutioiden summa: {summa}.")
