10dfcc03494f4f1b7377fa4772978192d1aa77e1
Elkkuss <eliastk@metropolia.fi>
2025-04-16 19:23:53 +0300

    lisäsin moduuli 3 tehtävä 2

---
+++ JavaScript/Moduuli3/T2/t2.js
@@ -0,0 +1,11 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const target = document.getElementById("target");
+
+    const items = ["First item", "Second item", "Third item"];
+
+    items.forEach(text => {
+        const li = document.createElement("li");
+        li.textContent = text;
+        target.appendChild(li);
+    });
+});
